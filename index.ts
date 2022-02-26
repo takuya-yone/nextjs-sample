c2aeb399555601fcc4008ff02678a2b8cb0550c2
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-02-27 02:30:10 +0900

    up

--- index.ts
+++ index.ts
@@ -1,54 +1,49 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express';
 
-import bodyParser from 'body-parser'
-import session  from 'express-session'
-import next from 'next'
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import next from 'next';
 
-
-const port = 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 
 //config
 log4js.configure({
-    appenders: {
-        system: {type: 'stdout'},
-        access: {type: 'stdout'} 
-    },
-    categories: {
-        default: {appenders:['system'], level: 'debug'},
-        web: {appenders: ['access'], level: 'info'} 
-    }
+  appenders: {
+    system: { type: 'stdout' },
+    access: { type: 'stdout' },
+  },
+  categories: {
+    default: { appenders: ['system'], level: 'debug' },
+    web: { appenders: ['access'], level: 'info' },
+  },
 });
 
 //logger
-var systemLogger = log4js.getLogger(); 
-var accessLogger = log4js.getLogger('web'); 
-
+var systemLogger = log4js.getLogger();
+var accessLogger = log4js.getLogger('web');
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
   server.use(log4js.connectLogger(accessLogger));
-  server.use(bodyParser.json())
+  server.use(bodyParser.json());
 
   server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   try {
     server.listen(port, () => {
-      console.log('server is running!!!')
-    })
+      console.log('server is running!!!');
+    });
   } catch (e) {
     if (e instanceof Error) {
-      console.error(e.message)
+      console.error(e.message);
     }
   }
-
-
-
-})
+});
