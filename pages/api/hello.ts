bad7db46dc71707b95c3c900a096161c151fcc87
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2022-03-14 19:05:27 +0900

    up

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,16 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
 type Data = {
   name: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  console.log('adfas;fasdf;ja');
-  const response = await fetch(`http://${process.env.NEXT_PUBLIC_APIHOST}/`);
-  const data = await response.json();
-  // console.log(data);
-  res.status(200).json(data);
+  await axios
+    .get(`http://${process.env.NEXT_PUBLIC_APIHOST}/`)
+    // thenで成功した場合の処理をかける
+    .then((response) => {
+      // console.log('status:', response.status); // 200
+      // console.log('body:', response.data);     // response body.
+      res.status(200).json(response.data);
+
+      // catchでエラー時の挙動を定義する
+    })
+    .catch((err) => {
+      console.log('err:', err);
+      res.status(200).json(err);
+    });
 }
