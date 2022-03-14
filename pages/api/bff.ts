37ee7c5de7d19fb438b7d097c0df8f2e921bde35
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-03-15 05:10:21 +0900

    up

--- pages/api/bff.ts
+++ pages/api/bff.ts
@@ -2,10 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
